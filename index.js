19326b5ad78224c52c577ec023722091d36707c4
saladNights <the.withered.fern@gmail.com>
2020-07-05 16:18:54 +0300

    cors

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const cors = require('cors');
 const port = 3001;
 
 app.use(cors());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", '*');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	next();
+});
 
 const AWS = require('aws-sdk');
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
